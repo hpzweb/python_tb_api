6ff4b4719d440224f3d01bd120578731c154de9d
Simon <simonq80@gmail.com>
2017-11-22 13:17:20 +0000

    rename etc

--- thingsboard_api.py
+++ thingsboard_api.py
@@ -1,9 +1,14 @@
 import requests
 
-class api:
+class thingsboard:
     def __init__(self, host, usr, pwd):
         self.host = 'http://' + host
-        payload = {"username": usr, "password": pwd}
+        self.usr = usr
+        self.pwd = pwd
+        self.get_auth_token()
+
+    def get_auth_token(self):
+        payload = {"username": self.usr, "password": self.pwd}
         resp = requests.post(self.host + '/api/auth/login', json=payload)
         resp.raise_for_status()
         self.auth_header = {'X-Authorization': 'Bearer ' + resp.json()['token']}
@@ -23,7 +28,8 @@ class api:
 
 if __name__ == "__main__":
 
-    a = api('localhost:8080', 'sysadmin@thingsboard.org', 'sysadmin')
+    a = thingsboard('localhost:8080', 'siquigle@tcd.ie', 'admin')
 
+    print(a.auth_header)
     print(a.get_request('/api/admin/updates'))
     print(a.post_request('/api/devices', {'deviceTypes': ['string']}))
